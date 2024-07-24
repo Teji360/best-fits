5edf64ce0d30f1d383f3793b785aae1c0b19a349
Teji360 <TejiriD.Aror@gmail.com>
2024-07-24 00:59:59 -0400

    Creating Hero and rendering title

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex items-center'>
+        <p className='text-4xl tracking-wide '>Share your fits with everyone</p>
+    </div>
+  )
+}
+
+export default Hero
