dd114cd00337aae216e99a426f8c9a560f5eb4f8
Teji360 <TejiriD.Aror@gmail.com>
2024-07-29 02:10:53 -0400

    Experimenting with utility functions

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import React from 'react'
+import sayHello from '@/utils/utils'
+
 
 function Hero() {
+ 
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center m-10'>
         <p className='text-4xl tracking-wide '>Share your fits with everyone</p>
+        <button onClick={sayHello}>Say Hello</button>
     </div>
   )
 }
