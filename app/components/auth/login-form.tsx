7bf091ab62b92dadaaeda5b88659670167c5bdf0
Teji360 <TejiriD.Aror@gmail.com>
2024-08-02 01:29:50 -0400

    Add more input type logic

--- app/components/auth/login-form.tsx
+++ app/components/auth/login-form.tsx
@@ -1,6 +1,33 @@
-import { CardWrapper } from "./card-wrapper";
+'use client'
 
-export const LoginForm = () => { //we do not export default because we are just exporting a single component not a page. 
+import * as z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+
+} from "@/components/ui/form"
+import { CardWrapper } from "./card-wrapper";
+import { resolve } from "path";
+import { Input } from "@/components/ui/input";
+
+export const LoginForm = () => { //we do not export default because we are just exporting a single component not a page.
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver : zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+    
     return (
         <CardWrapper
             headerLabel="Welcome Back"
@@ -8,7 +35,31 @@ export const LoginForm = () => { //we do not export default because we are just 
             backButtonHref="/auth/register"
             showSocial
             >
-            Login Form!
+             <Form {...form}>
+                <form 
+                onSubmit={form.handleSubmit(() => {})}
+                className="space-y-6"
+                >
+                    <div className="space-y-4">
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        placeholder="john.doe@example.com"
+                                        type="email"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
+                    </div>
+                </form>
+             </Form>
         </CardWrapper>
     )
 } 
