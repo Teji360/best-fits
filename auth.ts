b20a5c6d66fd00a50cbd3fa71698c3ef6ba0509f
Teji360 <TejiriD.Aror@gmail.com>
2024-08-06 02:10:44 -0400

    appending auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import authConfig from "./auth.config"
+import { db } from "@/lib/db"
 
 export const {
     handlers: { GET, POST },
     auth, //get current session or not seesion at all if the user is logged out
 } = NextAuth({
-    providers: [GitHub]
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
 })
